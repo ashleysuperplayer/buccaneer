3204e4397ae0fb305f4f2f53964bc0479097aa87
ashleysuperplayer <ashley.slater314@gmail.com>
2021-10-28 00:12:36 +0000

    checkKeys is an instantly obsolete function!

--- options.py
+++ options.py
@@ -1,18 +1,25 @@
 import os
 import butils
 
+dummyOptions = ('CJWPopupConfirmation', 'DefaultHeight', 'DefaultWidth')
+
 class OptionsHandler:
     def __init__(self):
         self.options = self.initOptions()
 
     def initOptions(self):
         self.options = {}
-        with open('/home/slater/store/coding/buccaneer/options.cfg') as optionLines:
+        with open('options.cfg') as optionLines:
             for line in optionLines:
                 lineComponents = validateOption(line)
                 self.options[lineComponents[0]] = lineComponents[1]
                 print(self.options[lineComponents[0]])
 
+    def checkKeys(self):
+        for dummyOption in dummyOptions:
+            if not dummyOption in self.options.keys():
+                print(dummyOption)
+                self.options[dummyOption] = 0
 
 def validateOption(optionLine):
     return splitOption(butils.removeWhitespace(butils.removeComments(optionLine)))
@@ -20,5 +27,6 @@ def validateOption(optionLine):
 def splitOption(string):
     return string.split("=")
 
-wagwan = OptionsHandler()
-wagwan.initOptions()
+# optionsHandler = OptionsHandler() testing
+# optionsHandler.initOptions()
+# optionsHandler.checkKeys()
